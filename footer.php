1e56e13715688d401496581fc6d3727205cc7107
Christopher Smikle <kaizenwp@users.noreply.github.com>
2018-03-22 07:37:12 -0500

    Added Footer Text

--- footer.php
+++ footer.php
@@ -1,3 +1,25 @@
+    <footer>
+      <?php
+          $args = array(
+            'theme_location' => 'header-menu',
+            'container' => 'nav',
+            'after' => '<span class="seporator"></span>'
+          );
+          wp_nav_menu($args);
+       ?>
+
+       <div class="location">
+         <p>779 Logwood Terace, Portmore</p>
+         <p>Phone Number: +1-876-872-8763</p>
+       </div>
+
+
+       <p class="copyright">All rights reserved <?php echo date('Y'); ?></p>
+    </footer>
+
+
+
+
 
     <?php wp_footer(); ?>
   </body>
